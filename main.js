e7d80a9fd1a75f5f710cd55d41886cb27f683c21
rasiyakaratt@gmail.com <rasiyakaratt@gmail.com>
2023-01-28 10:20:36 +0530

    documentation updated

--- main.js
+++ main.js
@@ -40,3 +40,18 @@ function menu() {
   menuCode += 1;
 }
 
+function htmlReplacer(v1, v2){
+  document.body.innerHTML = document.body.innerHTML.replace(v1, v2)
+}
+
+function htmlReplacerAll(v1, v2){
+  document.body.innerHTML = document.body.innerHTML.replaceAll(v1, v2)
+}
+
+window.onload = function (){
+  $('.load').style.display = 'none'
+}
+
+var mainApp = Mango()
+
+htmlReplacerAll(1.7, mainApp.dev_version)
